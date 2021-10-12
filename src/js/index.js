c7fc00afd2f1b205c516de73e51f15ffaeef627a
Kamil Lachowicz <kamil109111@gmail.com>
2021-10-12 17:57:52 +0200

    working app

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,19 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+
+const saveButton = document.querySelector('.save--js');
+const loadButton = document.querySelector('.load--js');
+const entryInput = document.querySelector('.entry--js');
+
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('entry', entryInput.value);
+})
+
+loadButton.addEventListener('click', () => {
+    const entry = localStorage.getItem('entry');
+    if (entry){
+        entryInput.innerHTML = entry;
+    }
+})
